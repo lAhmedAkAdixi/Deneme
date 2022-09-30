6f815906f312fdbd9f21bd03526d520138f4aa0b
Ahmet Akın <akin.ahmet@adixi.net>
2022-09-30 14:26:48 +0300

    git denemeleri

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DenemeProjer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
